f4121768787b38add01167ab524c0c536ab92cad
Artem0nn <96026159+Artem0nn@users.noreply.github.com>
2022-02-03 17:45:23 +0300

    Add files via upload

---
+++ Задача 8.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+void dfs(int v)
+{
+    stack<int> stack;
+
+    stack.push(v);
+
+    while (!stack.empty())
+    {
+        v = stack.top();
+        stack.pop();
+
+        if (used[v]) {
+            continue;
+        }
+
+        used[v] = true;
+        cout << v << "\n";
+
+        for (auto i = first; i != last; i++)
+        {
+            int u = *i;
+            if (!used[u]) {
+                stack.push(u);
+            }
+        }
+    }
+}
---
+++ Задача 9.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <fstream> 
+
+using namespace std;
+
+vector<vector<int>> g;
+vector<vector<int>> t;
+
+int n;
+int m;
+vector <int> color; 
+
+int Svyaznost()
+{
+    int c = 1;
+
+    for (int i = 0; i < m; i++)
+    {
+        if (color[t[i][0] - 1] == 0)
+        {
+            color[t[i][0] - 1] = c;
+            color[t[i][1] - 1] = c;
+
+            for (int j = 0; j < m; j++)
+            {
+                if ((t[j][0] == t[i][0]) || (t[j][0] == t[i][1]) || (t[j][1] == t[i][0]) || (t[j][1] == t[i][1]))
+                {
+                    color[t[j][0] - 1] = c;
+                    color[t[j][1] - 1] = c;
+                }
+            }
+
+            for (int j = 0; j < m; j++)
+            {
+                if (color[t[j][0] - 1] == c)
+                    color[t[j][1] - 1] = c;
+                if (color[t[j][1] - 1] == c)
+                    color[t[j][0] - 1] = c;
+            }
+            c++;
+        }
+    }
+
+    c--;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (color[i] == 0)
+        {
+            c++;
+        }
+    }
+
+    return c;
+}
+
+int main()
+{
+    vector <int> v;
+
+    ifstream fin("input.txt", ifstream::in);
+
+    if (fin)
+    {
+        fin >> n >> m;
+        int a;
+
+        for (int j = 0; j < m; j++)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                fin >> a;
+                v.push_back(a);
+            }
+            t.push_back(v);
+            v.clear();
+        }
+
+    }
+
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < 2; ++j)
+        {
+            cout << t[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+
+    color.resize(n);
+
+    cout << Svyaznost();
+}
